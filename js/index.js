ade7889219da7cc1702fbfb4a30dcc1a4ee0d935
jeaninerees <jeanine.rees@gmail.com>
2016-04-18 16:17:03 -0400

    Finally got it!

--- js/index.js
+++ js/index.js
@@ -1,45 +1,53 @@
 $(document).ready(function() {
 
-// State the variable, which is the text entered into the element with the "city-type" id.
-var city = $("#city-type").val();
-
 // State the function that will change the photo
 $("#form").submit(changePhoto)
 
 // Define the function
 function changePhoto(event) {
-//Keep the page from automatically reloading
+
+// Keep the page from automatically reloading
 	event.preventDefault();
 
-	if (city == "New York" || city == "NYC") {
+// Define the variable and its value	
+	var city = $("#city-type").val();
+
+// Do the if statements
+	if (city == "New York" || city == "NYC" || city == "The Big Apple") {
 	$("body").removeClass();	
 	$("body").addClass("nyc");
-	console.log("New York");
 
-	} else if (city == "San Francisco" || city == "Frisco") { 
+// Display a snappy statment in the console 
+	console.log("A helluva town");
+
+// Clear the form on submission
+	document.getElementById("form").reset();
+
+	} else if (city == "San Francisco" || city == "Frisco" || city == "Sanfran") { 
 	$("body").removeClass();
 	$("body").addClass("sf");
-	console.log("San Francisco");
+	console.log("Beware the tech bros");
+	document.getElementById("form").reset();
 
-	//} else if (city == "Los Angeles" || "LA" || "L.A." || "LAX")
-	//$("body").removeClass();
-	//$("body").addClass("la");
-	//console.log("El Ay");
+	} else if (city == "Los Angeles" || city == "LA" || city == "L.A." || city == "LAX") {
+	$("body").removeClass();
+	$("body").addClass("la");
+	console.log("Lala land");
+	document.getElementById("form").reset();
+
+	} else if (city == "Austin") {
+	$("body").removeClass();
+	$("body").addClass("austin");
+	console.log("Keep Austin weird");
+	document.getElementById("form").reset();
+ 
 	
-	//} else if (city == "Austin") {
-	//$("body").removeClass();
-	//$("body").addClass("austin");
-	//console.log("Keep Austin weird"); 
-	
-	//} else if (city == "Sydney") {
-	//$("body").removeClass();
-	//$("body").addClass("sydney");
-	//console.log("G'day");
-	}}
-
-
-
-
-
+	} else if (city == "Sydney") {
+	$("body").removeClass();
+	$("body").addClass("sydney");
+	console.log("G'day");
+	document.getElementById("form").reset();
+}
+ }
 
 }); 
